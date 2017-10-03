bd30f3b5957a58696e05854293aeeb140f5f446d
Mar Koss <kossmar2@gmail.com>
2017-10-03 13:57:56 -0700

    added user input and parsed it

--- main.m
+++ main.m
@@ -6,6 +6,19 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
      
         
+        char inputChar[256];
+        NSLog(@"enter a thing.");
+        
+        fgets(inputChar, 256, stdin);
+        
+        NSString *result = [NSString stringWithCString:inputChar encoding:NSUTF8StringEncoding];
+        
+        NSString *parsedResult = [result stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        
+        NSLog(@"%@", parsedResult);
+        
+        
+        
         
     }
     return 0;
